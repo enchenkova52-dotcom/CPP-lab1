7cbfb72f1d03687ed1a5c194f3acc00eae9d0283
Виолетта <enchenkova52@yandex.ru>
2026-05-06 21:12:09 +0300

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -1,8 +1,5 @@
 #include "Solution.h"
 
-Solution::Solution() : rootCount(0), x1(0.0), x2(0.0) {
-}
-
 Solution::Solution(int count, double r1, double r2)
     : rootCount(count), x1(r1), x2(r2) {
 }
